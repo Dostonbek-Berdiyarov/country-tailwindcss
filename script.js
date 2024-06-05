7f4d3f6164bb2e1359673d9b8d6d949ca27c276d
Dostonbek-Berdiyarov <dostonbey.0726@gmail.com>
2024-06-05 17:47:26 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const elThemeBtn = document.querySelector("#js-theme");
+
+elThemeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  darkMode: "selector",
+  theme: {
+    extend: {},
+    fontFamily: {
+      nunito: ['"Nunito Sans"'],
+    },
+    backgroundImage: {
+      moon: 'url("../images/moon.svg")',
+      darkmoon: 'url("../images/darkmoon.svg")',
+      search: 'url("../images/search.svg")',
+      darksearch: 'url("../images/darksearch.svg")',
+      arrow: 'url("../images/arrow.svg")',
+      darkarrow: 'url("../images/darkarrow.svg")',
+    },
+    boxShadow: {
+      header: " 0px 2px 4px 0px rgba(0, 0, 0, 0.06)",
+      input: " 0px 2px 9px 0px rgba(0, 0, 0, 0.05)",
+    },
+    backgroundPosition: {
+      searchpos: "left 32px center",
+      selectsm: "right 19px center",
+      selectlg: "right 18px center",
+    },
+    colors: {
+      headerbg: "#2B3844",
+      body: "#202C36",
+    },
+  },
+  plugins: [],
+};
